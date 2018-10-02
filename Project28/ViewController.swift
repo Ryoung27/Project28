16e7633bb365a320e353fa7ff8cde4cf790a1acc
Ryoung27 <RRYoung89@gmail.com>
2018-10-02 08:05:26 -0500

    Initial Commit

---
+++ Project28/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project28
+//
+//  Created by Richard Young on 10/2/18.
+//  Copyright © 2018 Richard Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
